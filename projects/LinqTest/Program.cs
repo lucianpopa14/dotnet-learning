2e14f63370ae20ad8fd36b182eaecafb657b4023
lucian popa <lucianpopa98@yahoo.com>
2025-11-22 20:25:33 +0200

    linq practice

---
+++ projects/LinqTest/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program()
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+
+        for (int i = 1; i <= 99; i++)
+        {
+            numbers.Add(i);
+        }
+
+        var evens = from number in numbers
+                    where number % 2 == 0
+                    select number;
+        foreach (int i in evens)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
