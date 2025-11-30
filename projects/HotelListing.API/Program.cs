a6fa92cbd1f6ff7ed4999f91caceef3fa7bf23f6
lucian popa <lucianpopa98@yahoo.com>
2025-11-30 15:29:16 +0200

    setup logging using serilog and deq

--- projects/HotelListing.API/Program.cs
+++ projects/HotelListing.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +17,7 @@ builder.Services.AddCors(options =>
         .AllowAnyMethod());
 });
 
-
+builder.Host.UseSerilog((ctx, lc)=>lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
