638024442947e51b6c427e911b2ab5aa6ea83b7d
lucian popa <lucianpopa98@yahoo.com>
2025-12-03 08:16:03 +0200

    db context

--- projects/HotelListing.API/Data/HotelListingDbContext.cs
+++ projects/HotelListing.API/Data/HotelListingDbContext.cs
@@ -11,5 +11,42 @@ namespace HotelListing.API.Data
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Germany",
+                    ShortName = "DE"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                }
+                );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Accor",
+                    Address = "accor address",
+                    CountryId = 1,
+                    Rating = 4
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort suites",
+                    Address = "Suites address",
+                    CountryId = 2,
+                    Rating = 5
+                }
+                );
+        }
     }
 }
