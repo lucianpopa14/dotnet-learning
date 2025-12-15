6e006df9dcf342c4489a62567fd482bbef9e4b82
Lucian Popa <lucianpopa98@yahoo.com>
2025-12-15 09:27:44 +0200

    adding authorization to endpoints

--- projects/HotelListing.API/Controllers/HotelsController.cs
+++ projects/HotelListing.API/Controllers/HotelsController.cs
@@ -2,6 +2,7 @@
 using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Hotel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace HotelListing.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class HotelsController : ControllerBase
     {
         private readonly IHotelsRepository _hotelsRepository;
